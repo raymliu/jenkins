aa052843ecc892fb4d5e9dcfc5f439d1adac210e
raymliu <ac11321@163.com>
2021-08-24 15:45:29 +0800

    update

--- build.sh
+++ build.sh
@@ -2,10 +2,9 @@
 
 current=`date "+%Y-%m-%d%H:%M:%S"`
 
-# shellcheck disable=SC2006
-time_tag=`date -d "$current" +%s`
 
-echo $time_tag
-docker build -t jenkins_test:$time_tag .
+
+
+docker build -t jenkins_test:1.0 .
 #
-docker run  --name=jenkins_test-$time_tag -p 8080:8080 -d jenkins_test:$time_tag
+docker run  --name=jenkins_test-1.0 -p 8080:8080 -d jenkins_test:1.0
